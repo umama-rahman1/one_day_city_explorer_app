2254cc7194de3de28be25d61ccb5f443680ba109
mepha89 <umamarahman12@gmail.com>
2023-10-13 10:42:34 -0230

    UR-002-set-up-image-and-text-tile

--- lib/itinerary_selection_screen.dart
+++ lib/itinerary_selection_screen.dart
@@ -7,12 +7,41 @@ class ItinerarySelectionScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Itinerary Selection')),
-        body: GridView.count(crossAxisCount: 2, children: const [
-          Text('Adventure Enthusiasts'),
-          Text('History Aficionados'),
-          Text('Culinary Seekers'),
-          Text('Nightlife Revelers')
+        body: GridView.count(crossAxisCount: 2, children: <Widget>[
+          //Create a tile for each category.
+          buildCategoryTile('Adventure Enthusiasts', 'assets/itinerary/adventure.jpg', () { // Navigate to the Adventure category screen
+          },),
+          buildCategoryTile('History Afiocionados', 'assets/itinerary/history.jpg', () { // Navigate to the History category screen
+          },),
+          buildCategoryTile('Culinary Seekers', 'assets/itinerary/culinary.jpg', () { // Navigate to the Culinary category screen
+          },),
+          buildCategoryTile('Nightlife Revelers', 'assets/itinerary/nightlife.jpg', () {// Navigate to the Nightlife category screen
+          },),
         ]));
   }
-}
 
+  // create a widget that builds a category tile with background image and title on foreground that user can click and go to next page with
+  Widget buildCategoryTile(String title, String backgroundImage, void Function()? onTap) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(backgroundImage),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
