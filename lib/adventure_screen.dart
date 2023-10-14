0237bb59f7a6dfafd5e357795e6c1d800fa56038
mepha89 <umamarahman12@gmail.com>
2023-10-14 11:00:46 -0230

    UR-003-activity-card-set-up

--- lib/adventure_screen.dart
+++ lib/adventure_screen.dart
@@ -8,45 +8,41 @@ class AdventureScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Adventure Enthusiasts')),
-        body: ListView(
-          children: <Widget>[
-            //create large tiles for activity suggestions.
-            // each tile should include a name, image and description
-            buildActivityTile(
-                'Grouse Grind',
-                'assets/itinerary/adventure.jpg',
-                //'assets/activity/grouse-grind.jpg',
-                'The Grouse Grind is a 2.9-kilometre (1.8 mi) trail up the face of Grouse Mountain, commonly referred to as "Mother Nature\'s Stairmaster". It is a steep and challenging hike that gains 853 m (2,800 ft) of elevation over its 2.9 km length.',
-                () {
-              // Navigate to the Grouse Grind activity screen
-            }),
-          ],
-        ));
+        body: ListView(children: <Widget>[
+          //create large tiles for activity suggestions.
+          // each tile should include a name, image and description
+          buildActivityTile(
+              'Grouse Grind',
+              'assets/itinerary/adventure.jpg',
+              //'assets/activity/grouse-grind.jpg',
+              'The Grouse Grind is a 2.9-kilometre (1.8 mi) trail up the face of Grouse Mountain, commonly referred to as "Mother Nature\'s Stairmaster". It is a steep and challenging hike that gains 853 m (2,800 ft) of elevation over its 2.9 km length.'),
+        ]));
   }
 }
 
 //create the buildActivityTile widget function
-Widget buildActivityTile(String title, String backgroundImage,
-    String description, void Function()? onTap) {
-  return InkWell(
-    onTap: onTap,
-    child: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(backgroundImage),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Center(
-        child: Text(
-          title,
+Widget buildActivityTile(
+    String title, String backgroundImage, String description) {
+  return Card(
+    elevation: 4.0,
+    child: Column(children: [
+      Text(title,
+          textAlign: TextAlign.center,
           style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
+            fontSize: 24,
+            color: Colors.black,
             fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    ),
+          )),
+      Container(
+          height: 200.0,
+          child: Ink.image(
+            image: AssetImage(backgroundImage),
+            fit: BoxFit.cover,
+          )),
+      Container(
+          padding: EdgeInsets.all(10.0),
+          alignment: Alignment.center,
+          child: Text(description)),
+    ]),
   );
 }
